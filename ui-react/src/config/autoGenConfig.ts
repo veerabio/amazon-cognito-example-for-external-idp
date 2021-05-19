3026f955bc108415b70bbcfa1f2a8d05ff99b176
veera <veera_cheedepudi-reddy@bio-rad.com>
2021-05-19 12:03:20 -0700

    add autoGenConfig without client ids

--- ui-react/src/config/autoGenConfig.ts
+++ ui-react/src/config/autoGenConfig.ts
@@ -1,9 +1,9 @@
 export default {
-    cognitoDomain: "brlabcloud.auth.us-west-2.amazoncognito.com",
+    cognitoDomain: "",
     region: "us-west-2",
-    cognitoUserPoolId: "us-west-2_TtX4ahV0f",
-    cognitoUserPoolAppClientId: "4g3tv3p3l0jrhfvcdvc2q3slh2",
+    cognitoUserPoolId: "",
+    cognitoUserPoolAppClientId: "",
     apiUrl: "https://yourapigwapiid.execute-api.region.amazonaws.com/prod/",
     appUrl: "http://localhost:3000",
-    identityPoolId: 'us-west-2:4509fd0f-ec4f-4ee3-baf7-e06cc32aa48c'
+    identityPoolId: ''
 };
